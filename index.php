474dd4ce36c95f93907d5bb6af66ac98ddc94eaa
Severin Kaderli <severin.kaderli@gmail.com>
2016-04-26 09:42:25 +0200

    Removed old routes

--- index.php
+++ index.php
@@ -10,34 +10,20 @@ $router->setBasePath(str_replace("http://" . $_SERVER['SERVER_NAME'], "", BASE_D
 /**
  * Defined routes
  */
-//Displaying of posts
-$router->addRoute("GET", "", "PostController@index");
-$router->addRoute("GET", "/{page}", "PostController@index");
-$router->addRoute("GET", "/post/{postId}", "PostController@show");
+// General
+$router->addRoute("GET", "", function(){echo "test";});
 
-//Authentication
+// Authentication
 $router->addRoute("GET", "/logout", "AuthController@logout");
 $router->addRoute("GET", "/login", "AuthController@showLogin");
 $router->addRoute("POST", "/login", "AuthController@login");
 $router->addRoute("GET", "/register", "AuthController@showRegister");
 $router->addRoute("POST", "/register", "AuthController@register");
 
-//Creating, editing and deleting of posts
-$router->addRoute("GET", "/create", "PostController@create");
-$router->addRoute("POST", "/post", "PostController@store");
-$router->addRoute("GET", "/post/{postId}/edit", "PostController@edit");
-$router->addRoute("POST", "/post/{postId}/update", "PostController@update");
-$router->addRoute("GET", "/post/{postId}/delete", "PostController@delete");
-
-//Comments
-$router->addRoute("POST", "/post/{postId}/comment", "CommentController@store");
-$router->addRoute("GET", "/comment/{commentId}/delete", "CommentController@delete");
-
-//User management
+// User management
 $router->addRoute("GET", "/users", "UserController@index");
 $router->addRoute("GET", "/user/{userId}/delete", "UserController@delete");
 $router->addRoute("GET", "/user/{userId}/promote", "UserController@promote");
-
 
 /**
  * Dispatching and call the matched method
