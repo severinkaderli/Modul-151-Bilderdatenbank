a0fd2683f39ea7f7ee49a23483fdf4c992ef4e0f
Severin Kaderli <severin.kaderli@gmail.com>
2016-05-03 09:31:24 +0200

    Added find method to the parent Model class.

--- Core/Model/Model.php
+++ Core/Model/Model.php
@@ -2,12 +2,59 @@
 
 namespace Core\Model;
 
+use Core\Database\DatabaseConnection;
+
 /**
-* @author Severin Kaderli
-*/
-abstract class Model
+ * @author Severin Kaderli
+ */
+class Model
 {
 
+    /**
+     * The database table for this model. This should be set in every model to
+     * the correct table.
+     *
+     * @var string
+     */
+    static protected $table = "";
 
-    public function __construct() {}
+    /**
+     * Model constructor.
+     */
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * Finds and returns an Entity by its id
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public static function find($id)
+    {
+        echo static::$table;
+        $result = DatabaseConnection::getResult("SELECT * FROM " . static::$table . " WHERE id=:id", [":id" => $id]);
+        if (empty($result)) {
+            return null;
+        }
+
+        $object = new static();
+        foreach ($result[0] as $property => $value) {
+            $object -> $property = $value;
+        }
+
+        return $object;
+    }
+
+    /**
+     * Returns the current model as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return (array)$this;
+    }
 }
